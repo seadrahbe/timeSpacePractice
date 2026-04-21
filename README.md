5ddfb4c4d699e9a2a49a3230ad85c8c0a8b1d441
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-04-21 08:11:32 -0700

    New Questions and JUnit Command

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Commit frequently! BOTH PARTNERS MUST CONTRIBUTE COMMITS! Alternate which comput
 In `Practice.java` fill in the time and space complexities for the already completed methods. Implement the unimplemented methods AND ADD UNIT TESTS for them in `PracticeTest.java`.
 
 When you are finished, one partner should make a PR against the original repository. BOTH PARTNERS MUST COPY THE SAME PR LINK TO CANVAS TO GET CREDIT.
+
+Tests not running properly in the VS Code extension? Use this command to run them from the command line:
+
+```
+javac -cp lib/junit-platform-console-standalone-1.12.0.jar src/*.java && java -jar lib/junit-platform-console-standalone-1.12.0.jar --class-path src --scan-class-path
+```
--- src/Practice.java
+++ src/Practice.java
@@ -3,10 +3,12 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Practice {
 
   // Time Complexity:
-  // Space Complexity: 
+  // Space Complexity:
+  // Remember to define your variables!
   public static List<Integer> findEvens(int[] array) {
     List<Integer> evens = new ArrayList<>();
     for (int num : array) {
@@ -17,8 +19,10 @@ public class Practice {
     return evens;
   }
 
+  // Assume a rectangular array where #rows <= #cols
   // Time Complexity:
   // Space Complexity: 
+  // Remember to define your variables!
   public static int sumDiagonal(int[][] matrix) {
     int sum = 0;
     for (int i = 0; i < matrix.length; i++) {
@@ -30,10 +34,9 @@ public class Practice {
 
   // Time Complexity: 
   // Space Complexity: 
-  // Does the 'T' look confusing? Consider refreshing on generic methods
-  // We'll revisit generics as a class later
+  // Remember to define your variables!
   public static <T> Map<T, Integer> countFrequencies(T[] array) {
-    HashMap<T, Integer> frequencies = new HashMap<>();
+    Map<T, Integer> frequencies = new HashMap<>();
     for (T val : array) {
       frequencies.put(val, frequencies.getOrDefault(val, 0) + 1);
     }
@@ -42,6 +45,7 @@ public class Practice {
 
   // Time Complexity: 
   // Space Complexity: 
+  // Remember to define your variables!
   public static List<Integer> evensToSquare(int n) {
     List<Integer> evens = new ArrayList<>();
     for(int i = 0; i <= n*n; i+=2) {
@@ -50,12 +54,71 @@ public class Practice {
     return evens;
   }
 
+  // Time Complexity:
+  // Space Complexity:
+  // Remember to define your variables!
+  public static List<Integer> concatVals(Map<Integer, List<Integer>> map) {
+    List<Integer> result = new ArrayList<>();
+
+    for(Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+      for(int num : entry.getValue()) {
+        result.add(num);
+      }
+    }
+    return result;
+  }
+
+  // Time Complexity:
+  // Space Complexity:
+  // Remember to define your variables!
+  public static List<Character> septuple(char c) {
+    List<Character> result = new ArrayList<>();
+    for(int i = 0; i < 7; i++) {
+      result.add(c);
+    }
+    return result;
+  }
+
+  // Time Complexity:
+  // Space Complexity:
+  // Remember to define your variables!
+  public static char[][] initCharRect(int rows, int cols) {
+    return new char[rows][cols];
+  }
+
+  // Assume all strings in strs are of length s
+  // Hint: charAt is an O(1) operation in both time and space
+  // Time Complexity:
+  // Space Complexity:
+  // Remember to define your variables!
+  public static List<Character> concatStrings(List<String> strs) {
+    List<Character> concat = new ArrayList<>();
+    for(String str : strs) {
+      for(int i = 0; i < str.length(); i++) {
+        concat.add(str.charAt(i));
+      }
+    }
+    return concat;
+  }
+
+  // Time Complexity:
+  // Space Complexity: 
+  // Remember to define your variables!
+  public static int cubic(int n, int a, int b, int c, int d) {
+    int result = d;
+    result += c * n;
+    result += b * n * n;
+    result += a * n * n * n;
+    return result;
+  }
+
+
   /**
    * Returns the integer that shows up most frequently in an array.
    * If there is a tie, tiebreak by returning the one that shows up first
    * in the array.
    * 
-   * THIS METHOD MUST RUN IN O(n) TIME. n = nums.size()
+   * THIS METHOD MUST RUN IN O(n) TIME. n = nums.length
    * 
    * Once you finish, WRITE TESTS FOR IT in PracticeTest.java
    * 
@@ -67,7 +130,7 @@ public class Practice {
    */
   public static int mostCommonTimeEfficient(int[] nums) {
     // TODO: Complete this method with an implementation that runs
-    // in O(n) time. n = nums.size()
+    // in O(n) time. n = nums.length
     return -1;
   }
 
