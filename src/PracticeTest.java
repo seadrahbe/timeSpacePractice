5a358e976800bfae56ae37a17cf4a5b0a524fb42
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-04-21 15:26:12 -0700

    Updated function for case of empty array, added test cases

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -6,5 +6,45 @@ public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
+  @Test
+  void testMostCommonTimeEfficient() {
+
+    int[] testArr = {1, -2, 3, 4, 1, -2, 1};
+
+    int actual = Practice.mostCommonTimeEfficient(testArr);
+
+    assertEquals(1, actual);
+  }
+
+  @Test
+  void testMostCommonTimeEfficientSame() {
+
+    int[] testArr = {1, 2, 1, 2, 1, 2};
+
+    int actual = Practice.mostCommonTimeEfficient(testArr);
+
+    assertEquals(1, actual);
+  }
+
+  @Test
+  void testMostCommonTimeEfficientEmpty() {
+
+    int[] testArr = {};
+
+    int actual = Practice.mostCommonTimeEfficient(testArr);
+
+    assertEquals(0, actual);
+  }
+
+  @Test
+  void testMostCommonTimeEfficientOneValue() {
+
+    int[] testArr = {1};
+
+    int actual = Practice.mostCommonTimeEfficient(testArr);
+
+    assertEquals(1, actual);
+  }
+
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
 }
