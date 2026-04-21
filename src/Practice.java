0c0e2d533acab9e0d1295033982cbdfeb31a07ad
AaronB22 <aaronthesnowboarder@live.com>
2026-04-21 11:40:25 -0700

    1st q

--- src/Practice.java
+++ src/Practice.java
@@ -6,9 +6,9 @@ import java.util.Map;
 
 public class Practice {
 
-  // Time Complexity:
-  // Space Complexity:
-  // Remember to define your variables!
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  // n= array.length
   public static List<Integer> findEvens(int[] array) {
     List<Integer> evens = new ArrayList<>();
     for (int num : array) {
